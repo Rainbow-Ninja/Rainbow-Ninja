44af5d4f76ccc21bb9f0cad3ef464979503f3a7d
Jo Dunham <scaffelpike@yahoo.com.au>
2020-11-25 14:56:19 +1100

    fixed typo

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,14 +1,14 @@
 multiplicationTable = function(size) {
     const table = [];
     let arr = []
-    for(i = 1; i <=size; i++){
+    for(i = 1; i <= size; i++){
         for(j = 1; j <= size; j++){
             arr.push(j*i);
         }
     table.push(arr);
     arr = []
     }
-    console.log(table);
+    return table;
 }
 
 multiplicationTable(3);
