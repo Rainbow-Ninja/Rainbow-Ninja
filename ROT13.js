a26569b22ec0e2b6927392d6bfba7bd7c917109f
Jo Dunham <scaffelpike@yahoo.com.au>
2020-12-14 10:23:26 +1100

    ROT13

---
+++ ROT13.js
@@ -0,0 +1,24 @@
+// How can you tell an extrovert from an introvert at NSA? Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+
+// I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it? According to Wikipedia, ROT13 (http://en.wikipedia.org/wiki/ROT13) is frequently used to obfuscate jokes on USENET.
+
+// Hint: For this task you're only supposed to substitue characters. Not spaces, punctuation, numbers etc. Test examples:
+
+// rot13("EBG13 rknzcyr.") == "ROT13 example.";
+// rot13("This is my first ROT13 excercise!" == "Guvf vf zl svefg EBG13 rkprepvfr!"
+
+function rot13(str) {
+    const arrSub = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+    strArr = str.split("");
+    rot13Str = strArr.map( letter => {
+        const lett = arrSub.indexOf(letter)
+        if(lett == -1) { return letter }
+        let sub = lett + 13
+        if(sub > 26) { sub = sub - 26 }
+        return arrSub[sub]
+    })
+    return rot13Str.join("")
+}
+
+
+console.log(rot13("hello world"))
